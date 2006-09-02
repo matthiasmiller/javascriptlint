f335a63c97b8ab236480dc51a3be3d315dca0af8
matthiasmiller <matthiasmiller@19aaaf21-a014-0410-a683-efa9e2540a82>
2006-09-02 23:03:11 +0000

    more test reorganization

--- trunk/tests/warnings/inc_dec_within_stmt.js
+++ trunk/tests/warnings/inc_dec_within_stmt.js
@@ -2,38 +2,62 @@
 function inc_dec_within_stmt() {
     var i, s;
 
-    /* legal */
-    i++;
-    i--;
-    ++i;
-    --i;
-    for (i = 0; i < 10; i++) {
-        s = i;
-    }
-    for (i = 10; i > 0; i--) {
-        s = i;
-    }
-    for(i = 0; i < 5; i++, i--) {
+    function statements() {
         i++;
-    }
-    for(i = 0; i < 5; ) {
-        i++;
-    }
-
-    for (i = 0; i < 5; i = ++i) { /*warning:inc_dec_within_stmt*/
-        /*jsl:pass*/
+        i--;
+        ++i;
+        --i;
     }
 
-    /* illegal */
-    switch (i--) /*warning:inc_dec_within_stmt*/ 
-    {
-    default:
-        break;
+    function for_loops() {
+        for (i = 0; i < 10; i++) {
+            s = i;
+        }
+        for (i = 10; i > 0; i--) {
+            s = i;
+        }
+        for(i = 0; i < 5; i++, i--) {
+            i++;
+        }
+        for(i = 0; i < 5; ) {
+            i++;
+        }
+
+        for (i = 0; i < 5; i = ++i) { /*warning:inc_dec_within_stmt*/
+            /*jsl:pass*/
+        }
     }
 
-    /* illegal */
-    s = new String(i++); /*warning:inc_dec_within_stmt*/
+    function expressions() {
+        switch (i--) /*warning:inc_dec_within_stmt*/ 
+        {
+        default:
+            break;
+        }
 
-    /* illegal */
-    s = --i; /*warning:inc_dec_within_stmt*/
+        s = new String(i++); /*warning:inc_dec_within_stmt*/
+
+        s = --i; /*warning:inc_dec_within_stmt*/
+    }
+
+    function jsl_ignore() {
+        var x;
+        do {
+            var y = x--; /*warning:inc_dec_within_stmt*/
+        } while (x > 0);
+
+        do y = x--; /*warning:inc_dec_within_stmt*/
+        while (x > 0);
+
+        do {
+            /*jsl:ignore*/
+            var y = x--;
+            /*jsl:end*/
+        } while (x > 0);
+
+        Label--: /*warning:bad_label*/
+        do {
+           x++;
+        } while (x < 0);
+    }
 }
