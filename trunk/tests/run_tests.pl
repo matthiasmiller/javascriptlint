30aab36c98c0fca3fcdaa5c7a66b89b4ddf7bf37
matthiasmiller <matthiasmiller@19aaaf21-a014-0410-a683-efa9e2540a82>
2006-09-02 22:49:19 +0000

    move tests for warnings into a subdirectory

--- trunk/tests/run_tests.pl
+++ trunk/tests/run_tests.pl
@@ -1,43 +1,39 @@
 #!/usr/bin/perl
 
 use strict;
+use File::Find;
 use File::Spec;
-
-my ($script_volume,$script_directories,$script_file) = File::Spec->splitpath($0);
-my $program_dir = File::Spec->catpath($script_volume,$script_directories,'');
+use FindBin;
 
 # require a path to jsl
 #
 if (scalar(@ARGV) != 1) {
-	die("Usage: $script_file <path to jsl>\n");
+	die("Usage: run_tests.pl <path to jsl>\n");
 }
-my $jsl_path = $ARGV[0];
+my $jsl_path = File::Spec->rel2abs($ARGV[0]);
 
-# locate all files in the test folder
-#
-opendir(DIR, $program_dir) or die("Could not open directory $program_dir: $!");
-my @files = grep(/\.(js|htm|html)$/,readdir(DIR));
-closedir(DIR);
-
+my $num_tests = 0;
 my $num_passed = 0;
-foreach my $filename (@files) {
-	my $full_path = File::Spec->catpath($script_volume,$script_directories,$filename);
-	my $conf_path = File::Spec->catpath($script_volume,$script_directories,".jsl.conf");
+sub TestFile {
+	/\.(js|htm|html)$/ or return;
+	my $filename = $_;
+
+	my $conf_file = ".jsl.conf";
 
 	# open the path being validated
-	open(FILE, $full_path) or die("Could not open $full_path: $!");
+	open(FILE, $filename) or die("Could not open $filename: $!");
 	my @contents = <FILE>;
 
 	# look for special configuration directives
 	my @conf = grep(s/\/\*conf:([^*]*)\*\//\1\n/g, @contents);
-	open(FILE, ">$conf_path") or die("Could not open configuration file $conf_path: $!");
+	open(FILE, ">$conf_file") or die("Could not open configuration file $conf_file: $!");
 	print FILE join("",@conf);
 	close FILE;
 
 	# run the lint
 	print "Testing $filename...\n";
-	my $results = `$jsl_path --conf $conf_path --process $full_path --nologo --nofilelisting --nocontext --nosummary -output-format __LINE__,__ERROR_NAME__`;
-	unlink $conf_path;
+	my $results = `$jsl_path --conf $conf_file --process $filename --nologo --nofilelisting --nocontext --nosummary -output-format __LINE__,__ERROR_NAME__`;
+	unlink $conf_file;
 	die "Error executing $jsl_path" unless defined $results;
 
 	my $this_passed = 1;
@@ -61,7 +57,15 @@ foreach my $filename (@files) {
 	}
 	close(FILE);
 
+	$num_tests++;
 	$num_passed++ if $this_passed;
 }
 
-print "Passed $num_passed of ", scalar(@files), " tests\n";
+# locate all files in the test folder
+#
+my @dirs;
+push(@dirs, $FindBin::Bin);
+print "Searching $FindBin::Bin...\n";
+find( sub{TestFile}, '.');
+
+print "Passed $num_passed of $num_tests tests\n";
