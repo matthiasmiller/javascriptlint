cccd1bf46928e173e9ff1c0440365b5b1fa4169c
matthiasmiller <matthiasmiller@19aaaf21-a014-0410-a683-efa9e2540a82>
2007-08-25 22:51:11 +0000

    resolve file paths immediately, rather than waiting until processing the script

---
+++ trunk/tests/path_resolution/is_a_file_not_dir.js
@@ -0,0 +1,3 @@
+/*conf:+process file/ */
+/*conf_error:unable to resolve path: file/*/
+/*jsl:pass*/
---
+++ trunk/tests/path_resolution/not_a_dir.js
@@ -0,0 +1,3 @@
+/*conf:+process not_a_dir*/
+/*conf_error:unable to resolve path: not_a_dir*/
+/*jsl:pass*/
