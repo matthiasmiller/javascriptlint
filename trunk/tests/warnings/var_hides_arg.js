926996982eb52b0f660f5322df65365d1789bf46
matthiasmiller <matthiasmiller@19aaaf21-a014-0410-a683-efa9e2540a82>
2008-08-23 21:58:12 +0000

    var_hides_arg: add test for possible enhancement

--- trunk/tests/warnings/var_hides_arg.js
+++ trunk/tests/warnings/var_hides_arg.js
@@ -1,4 +1,7 @@
 /*jsl:option explicit*/
-function var_hides_arg(duplicate) {
-    var duplicate; /*warning:var_hides_arg*/
+function var_hides_arg(duplicate1, duplicate2) {
+    var duplicate1; /*warning:var_hides_arg*/
+    function inner() {
+        var duplicate2; /*warning:var_hides_arg*/
+    }
 }
