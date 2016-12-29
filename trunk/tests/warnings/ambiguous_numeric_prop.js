4e674ac2d1ae52bbebde51ccbd5d42196c2839ac
matthiasmiller <matthiasmiller@19aaaf21-a014-0410-a683-efa9e2540a82>
2016-12-29 21:46:51 +0000

    Add test for the "ambiguous_numeric_prop" warning.

---
+++ trunk/tests/warnings/ambiguous_numeric_prop.js
@@ -0,0 +1,15 @@
+function ambiguous_numeric_prop() {
+    var a = {
+        1: '',
+        2.0: '', /*warning:ambiguous_numeric_prop*/
+        2.1: '',
+        2.2: '',
+        0x3: '' /*warning:ambiguous_numeric_prop*/
+    };
+
+    a[1] = '';
+    a[2.0] = ''; /*warning:ambiguous_numeric_prop*/
+    a[2.1] = '';
+    a[0x3] = ''; /*warning:ambiguous_numeric_prop*/
+}
+
